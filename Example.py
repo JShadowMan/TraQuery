224b2d092f1a4050183b5be8796f912fd703bb34
JShadowMan <jshadowman@163.com>
2016-06-13 16:14:54 +0800

    <feat>(eg): example update

--- Example.py
+++ Example.py
@@ -5,6 +5,13 @@ Created on 2016年6月10日
 @author: ShadowMan
 '''
 
-import TraQuery.TraQuery as Train
+import TraQuery.TraQuery as TraQuery
 
-print Train.adult('福鼎', '上海虹桥', '2016/07/08')
+march = TraQuery.adult('福州', '北京', '2016/07/08')
+
+print march.trainCount()
+
+print march.trainCodes()
+
+for trainCode in march.trainCodes():
+    print trainCode, march.selectTrainClass(trainCode), march.SelectSeat(trainCode), march.purchaseFlag(trainCode)
