82a827e5d7c0693eea7b65bdc40f668f262f87da
JShadowMan <jshadowman@163.com>
2016-06-20 12:32:38 +0800

    <fix>(TraQuery): fix a fatal error.

--- TraQuery/TraQuery.py
+++ TraQuery/TraQuery.py
@@ -49,7 +49,7 @@ SEAT_RZ  = 'softSeat'           # 软座
 SEAT_YZ  = 'hardSeat'           # 硬座
 SEAT_GR  = 'advancedSoft'       # 高级软卧
 
-
+# Error Exception Class
 class QueryError(IOError):
     def __init__(self, reason, code = 0):
         self.reason = reason
@@ -63,38 +63,37 @@ class QueryError(IOError):
 
 # Getting Train Station List From 12306
 class TraStationList(object):
-    _base_url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version='
+    _stationList_url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version='
 
     def __init__(self, stationVersion = '1.8954'):
-        self.__stationList = {}
+        self.__stationList = {} # TODO. static member, static class
         self.__initTraStationList(stationVersion)
 
     def __initTraStationList(self, stationVersion):
         try:
-            handler = urllib2.urlopen(self._base_url + stationVersion)
+            handler = urllib2.urlopen(self._stationList_url + stationVersion)
         except urllib2.URLError:
             if _have_ssl == True and '_create_unverified_context' in dir(ssl):
-                handler = urllib2.urlopen(self._base_url + stationVersion,  context = ssl._create_unverified_context())
+                handler = urllib2.urlopen(self._stationList_url + stationVersion, context = ssl._create_unverified_context())
             else:
                 raise QueryError('Require SSL Module')
 
         if handler.getcode() == 200:
-            self.__parseStation(handler.read())
+            self.__parseStationList(handler.read())
         else:
-            raise QueryError("Get Request Error Occurs.")
+            raise QueryError("Get StationList Error Occurs.")
 
-    def __parseStation(self, stationList):
+    def __parseStationList(self, stationList):
         stationList = stationList.decode('utf-8')
-        stationList = stationList.replace('  ', '') # f**k 12306
-        stationList = re.findall('([^@a-z|,;\'(\d+)_= ]+)', stationList)
+        stationList = re.findall('([^@a-z|,;\'(\d+)_= ]+)', stationList.replace('  ', ''))
 
         if (len(stationList) % 2) != 0:
-            raise QueryError("Get Station List Error Occurs")
+            raise QueryError("StationList Non Match")
 
         index = 0
         while index < len(stationList):
             self.__stationList[stationList[index]] = stationList[index + 1]
-            index = index + 2
+            index += 2
 
     def code(self, station):
         if type(station) == type(''):
@@ -108,132 +107,12 @@ class TraStationList(object):
     def contains(self, code):
         return code in self.__stationList.values()
 
+# TraStationList Instance
 traStationList = TraStationList()
-class TraQuery(object):
-    # Private Members
-    __queryTrainUrl = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=%s'
-    __queryStackUrl = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=%s&queryDate=%s&from_station=%s&to_station=%s'
 
-    def __init__(self):
-        self.__result = { 'trainCount': None, 'trains': {} }
-
-    def letsGo(self, fromStation, toStation, date, erType = ERTYPE_ADULT):
-        try:
-            fromStation = fromStation if traStationList.contains(fromStation) else traStationList.code(fromStation)
-            toStation   = toStation if traStationList.contains(toStation) else traStationList.code(toStation)
-            trainDate   = self.__parseDate(date)
-            erType      = self.__parseErType(erType)
-
-            self.__query(trainDate, fromStation, toStation, erType)
-        except QueryError, e:
-            raise e
-
-        return TraResult(self.__result)
-
-    def __parseDate(self, date):
-        if date.find(':') and date.count(':') == 2:
-            date = time.strptime(date, '%Y:%m:%d')
-        elif date.find('-') and date.count('-') == 2:
-            date = time.strptime(date, '%Y-%m-%d')
-        elif date.find('/') and date.count('/') == 2:
-            date = time.strptime(date, '%Y/%m/%d')
-        elif date.find('.') and date.count('.') == 2:
-            date = time.strptime(date, '%Y.%m.%d')
-        else:
-            raise QueryError('Date Format Error Occurs')
-
-        if time.mktime(date) < time.time() and time.mktime(date) < time.time() - (time.time() % 86400) - 8 * 3600:
-            raise QueryError('Date Error Occurs')
-
-        return time.strftime('%Y-%m-%d', date)
-
-    def __parseErType(self, erType):
-        if erType in (ERTYPE_ADULT, ERTYPE_STUDENT):
-            return erType
-        else:
-            return ERTYPE_ADULT
-
-    def __query(self, trainDate, fromStation, toStation, erType):
-        contents = self.__sendRequest(self.__queryTrainUrl % ( trainDate, fromStation, toStation, erType ))
-
-        self.__parseResult(contents, trainDate, erType)
-
-    def __sendRequest(self, url):
-        try:
-            handler = urllib2.urlopen(url)
-        except urllib2.URLError:
-            if _have_ssl == True and '_create_unverified_context' in dir(ssl):
-                handler = urllib2.urlopen(url,  context = ssl._create_unverified_context())
-            else:
-                raise QueryError('Require SSL Module')
-
-        if handler.getcode() == 200:
-            return handler.read().decode('utf8')
-        else:
-            raise QueryError('Request Error Occurs')
-
-    def __parseResult(self, contents, date, erType):
-        contents = json.loads(contents)
-
-        self.__result['trainCount'] = len(contents['data'])
-        if len(contents['data']) == 0:
-            raise QueryError('Not Train Data')
-        self.__result['erType'] = erType
-        self.__result['date'] = date
-
-        stackContent = self.__sendRequest(self.__queryStackUrl % ( erType, date, contents['data'][0]['queryLeftNewDTO']['from_station_telecode'], contents['data'][0]['queryLeftNewDTO']['to_station_telecode'] ))
-        stackContent = json.loads(stackContent)
-        stackContent = stackContent['data']['datas']
-
-        for train in contents['data']:
-            trainInfo = train['queryLeftNewDTO']
-
-            self.__result['trains'][ trainInfo['station_train_code'] ] = {
-                # Train Information
-                'train': { 'code': trainInfo['station_train_code'], 'class': trainInfo['station_train_code'][0], 'no': trainInfo['train_no'] },
-                # Station Information
-                'station': { 'from': trainInfo['from_station_name'], 'to': trainInfo['end_station_name'], 'fromNo': trainInfo['from_station_no'], 'fromCode': trainInfo['from_station_telecode'], 'toCode': trainInfo['to_station_telecode'], 'toNo': trainInfo['to_station_no'] },
-                # Could Purchase Flag
-                'purchase': trainInfo['canWebBuy'] == 'Y',
-                # Time Information
-                'time': { 'start': trainInfo['start_time'], 'end': trainInfo['arrive_time'], 'total': trainInfo['lishi'] },
-                # Seat Price
-                'price': {},
-                # Stack Count
-                'stack': self.__getStack(stackContent, trainInfo['train_no']),
-                # All Station
-                'allStation': {},
-                # Seat Type
-                'seatType': trainInfo['seat_types']
-            }
-
-    def __getStack(self, contents, stationNo):
-        train = None
-        for trainCode in contents:
-            if trainCode['train_no'] == stationNo:
-                train = trainCode
-                break
-
-        return {'first': self.__fromListGet(train, 'swz_num'), # 商务
-                'business': self.__fromListGet(train, 'zy_num'), # 一等座
-                'economy': self.__fromListGet(train, 'ze_num'), # 二等座
-                'none': self.__fromListGet(train, 'wz_num'), # 无座
-                'hardSeat': self.__fromListGet(train, 'yz_num'), # 硬座
-                'softSeat': self.__fromListGet(train, 'rz_num'), # 软座
-                'semiCushionedBerth': self.__fromListGet(train, 'yw_num'), # 硬卧
-                'cushionedBerth': self.__fromListGet(train, 'rw_num'), # 软卧
-                'advancedSoft': self.__fromListGet(train, 'gr_num') # 高级软卧
-            }
-
-    def __fromListGet(self, lst, key):
-        if key in lst:
-            if lst[key] == u'--' or lst[key] == u'无':
-                return None
-
-            return lst[key]
-        else:
-            return None
-
+''' TraSelect
+    DOCs
+'''
 class TraSelect(object):
     # Private: Select URL
     __queryPriceUrl = 'https://kyfw.12306.cn/otn/leftTicket/queryTicketPrice?train_no=%s&from_station_no=%s&to_station_no=%s&seat_types=%s&train_date=%s'
@@ -389,9 +268,136 @@ class TraResult(object):
 
         return dic
 
+class TraQuery(object):
+    # Private Members
+    __queryTrainUrl = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=%s'
+    __queryStackUrl = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=%s&queryDate=%s&from_station=%s&to_station=%s'
 
+    def __init__(self):
+        self.__result = { 'trainCount': None, 'trains': {} } # Single User, Fix This
 
-traQuery = TraQuery()
+    def letsGo(self, fromStation, toStation, date, erType = ERTYPE_ADULT):
+        try:
+            fromStation = fromStation if traStationList.contains(fromStation) else traStationList.code(fromStation)
+            toStation   = toStation if traStationList.contains(toStation) else traStationList.code(toStation)
+            trainDate   = self.__parseDate(date)
+            erType      = self.__parseErType(erType)
+
+            self.__clean()
+            self.__query(trainDate, fromStation, toStation, erType)
+        except QueryError, e:
+            raise e
+
+        return TraResult(self.__result)
+
+    def __parseDate(self, date):
+        if date.find(':') and date.count(':') == 2:
+            date = time.strptime(date, '%Y:%m:%d')
+        elif date.find('-') and date.count('-') == 2:
+            date = time.strptime(date, '%Y-%m-%d')
+        elif date.find('/') and date.count('/') == 2:
+            date = time.strptime(date, '%Y/%m/%d')
+        elif date.find('.') and date.count('.') == 2:
+            date = time.strptime(date, '%Y.%m.%d')
+        else:
+            raise QueryError('Date Format Error Occurs')
+
+        if time.mktime(date) < time.time() and time.mktime(date) < time.time() - (time.time() % 86400) - 8 * 3600:
+            raise QueryError('Date Error Occurs')
+
+        return time.strftime('%Y-%m-%d', date)
+
+    def __parseErType(self, erType):
+        if erType in (ERTYPE_ADULT, ERTYPE_STUDENT):
+            return erType
+        else:
+            return ERTYPE_ADULT
+
+    def __query(self, trainDate, fromStation, toStation, erType):
+        contents = self.__sendRequest(self.__queryTrainUrl % ( trainDate, fromStation, toStation, erType ))
+
+        self.__parseResult(contents, trainDate, erType)
+
+    def __sendRequest(self, url):
+        try:
+            handler = urllib2.urlopen(url)
+        except urllib2.URLError:
+            if _have_ssl == True and '_create_unverified_context' in dir(ssl):
+                handler = urllib2.urlopen(url,  context = ssl._create_unverified_context())
+            else:
+                raise QueryError('Require SSL Module')
+
+        if handler.getcode() == 200:
+            return handler.read().decode('utf8')
+        else:
+            raise QueryError('Request Error Occurs')
+
+    def __parseResult(self, contents, date, erType):
+        contents = json.loads(contents)
+
+        self.__result['trainCount'] = len(contents['data'])
+        if len(contents['data']) == 0:
+            raise QueryError('Not Train Data')
+        self.__result['erType'] = erType
+        self.__result['date'] = date
+
+        stackContent = self.__sendRequest(self.__queryStackUrl % ( erType, date, contents['data'][0]['queryLeftNewDTO']['from_station_telecode'], contents['data'][0]['queryLeftNewDTO']['to_station_telecode'] ))
+        stackContent = json.loads(stackContent)
+        stackContent = stackContent['data']['datas']
+
+        for train in contents['data']:
+            trainInfo = train['queryLeftNewDTO']
+
+            self.__result['trains'][ trainInfo['station_train_code'] ] = {
+                # Train Information
+                'train': { 'code': trainInfo['station_train_code'], 'class': trainInfo['station_train_code'][0], 'no': trainInfo['train_no'] },
+                # Station Information
+                'station': { 'from': trainInfo['from_station_name'], 'to': trainInfo['end_station_name'], 'fromNo': trainInfo['from_station_no'], 'fromCode': trainInfo['from_station_telecode'], 'toCode': trainInfo['to_station_telecode'], 'toNo': trainInfo['to_station_no'] },
+                # Could Purchase Flag
+                'purchase': trainInfo['canWebBuy'] == 'Y',
+                # Time Information
+                'time': { 'start': trainInfo['start_time'], 'end': trainInfo['arrive_time'], 'total': trainInfo['lishi'] },
+                # Seat Price
+                'price': {},
+                # Stack Count
+                'stack': self.__getStack(stackContent, trainInfo['train_no']),
+                # All Station
+                'allStation': {},
+                # Seat Type
+                'seatType': trainInfo['seat_types']
+            }
+
+    def __getStack(self, contents, stationNo):
+        train = None
+        for trainCode in contents:
+            if trainCode['train_no'] == stationNo:
+                train = trainCode
+                break
+
+        return {'first': self.__fromListGet(train, 'swz_num'), # 商务
+                'business': self.__fromListGet(train, 'zy_num'), # 一等座
+                'economy': self.__fromListGet(train, 'ze_num'), # 二等座
+                'none': self.__fromListGet(train, 'wz_num'), # 无座
+                'hardSeat': self.__fromListGet(train, 'yz_num'), # 硬座
+                'softSeat': self.__fromListGet(train, 'rz_num'), # 软座
+                'semiCushionedBerth': self.__fromListGet(train, 'yw_num'), # 硬卧
+                'cushionedBerth': self.__fromListGet(train, 'rw_num'), # 软卧
+                'advancedSoft': self.__fromListGet(train, 'gr_num') # 高级软卧
+            }
+
+    def __fromListGet(self, lst, key):
+        if key in lst:
+            if lst[key] == u'--' or lst[key] == u'无':
+                return None
+
+            return lst[key]
+        else:
+            return None
+
+    def __clean(self):
+        self.__result.clear()
+        self.__result = { 'trainCount': None, 'trains': {} }
+
 '''Public Function: adult(fromStation, toStation, date)
 @param fromStation: Departure Station
 @param toStation: Terminal Station
@@ -404,7 +410,8 @@ traQuery = TraQuery()
 @return: TraResult
 '''
 def adult(fromStation, toStation, date):
-    return traQuery.letsGo(fromStation, toStation, date, ERTYPE_ADULT)
+    # Single Instance
+    return TraQuery().letsGo(fromStation, toStation, date, ERTYPE_ADULT)
 
 
 '''Public Function: student(fromStation, toStation, date)
@@ -419,5 +426,5 @@ def adult(fromStation, toStation, date):
 @return: TraResult
 '''
 def student(fromStation, toStation, date):
-    return traQuery.letsGo(fromStation, toStation, date, ERTYPE_STUDENT)
+    return TraQuery().letsGo(fromStation, toStation, date, ERTYPE_STUDENT)
 
